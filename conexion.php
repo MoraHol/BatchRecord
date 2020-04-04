56cfa09a692c9106ae0e321249c9f047a2cba530
svelandi <55334171+svelandi@users.noreply.github.com>
2020-04-04 10:29:45 -0500

    Actualizacion conexion DB

--- conexion.php
+++ conexion.php
@@ -2,8 +2,7 @@
 $servername = "localhost";
 $database = "teenuste_batchrecord";
 $username = "teenuste_prueba";
-$username = "teenustest_prueba";
-$password = "HPCompu08**";
+$password = "XnENOtLLEJmr";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
