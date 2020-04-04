4dfe8e7d532d2f7b441e3b2e9ffbd221080058e0
svelandi <55334171+svelandi@users.noreply.github.com>
2020-04-04 10:21:34 -0500

    actualizacion password

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $database = "teenuste_batchrecord";
 $username = "teenuste_prueba";
-$username = "teenustest_prueba;
+$username = "teenustest_prueba";
 $password = "HPCompu08**";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
